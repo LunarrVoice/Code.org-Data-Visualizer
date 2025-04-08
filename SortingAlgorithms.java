3a0d68233941f43fcbdcb9198c0be1f4bf3032e7
Lunar <161249671+LunarrVoice@users.noreply.github.com>
2025-04-07 21:23:53 -0400

    Create SortingAlgorithms.java

---
+++ SortingAlgorithms.java
@@ -0,0 +1,21 @@
+import org.code.theater.*;
+import org.code.media.*;
+import java.util.*;
+
+public class SortingAlgorithms {
+
+  public SortingAlgorithms() {
+    
+  }
+
+  public ArrayList<Integer> selectionSort() {
+    ArrayList<Integer> temp = new ArrayList<Integer>(3);
+    return temp;
+  }
+
+  public ArrayList<Integer> insertionSort() {
+    ArrayList<Integer> temp = new ArrayList<Integer>(3);
+    return temp;
+  }
+  
+}
