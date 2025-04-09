295c8e1ac285d208ac82c8e8bdbc0451721a7420
Lunar <161249671+LunarrVoice@users.noreply.github.com>
2025-04-08 20:48:37 -0400

    added randomListEqualStep function

--- Functions.java
+++ Functions.java
@@ -8,6 +8,7 @@ public class Functions {
     
   }
 
+  // Creates an unsorted list with random values
   public ArrayList<Integer> randomList(int min, int max, int length) {
     ArrayList<Integer> list = new ArrayList<Integer>(length);
 
@@ -20,6 +21,32 @@ public class Functions {
     return list;
   }
 
+  // Creates an unsorted list of values counting up
+  public ArrayList<Integer> randomListEqualSteps(int length) {
+    ArrayList<Integer> list = new ArrayList<Integer>(length);
+
+    for (int i = 0; i < length; i++) {
+      list.add(i + 1);
+    }
+
+    return randomizeList(list);
+  }
+
+  // Randomizes a sorted list
+  public ArrayList<Integer> randomizeList(ArrayList<Integer> list) {
+    ArrayList<Integer> randomizedList = new ArrayList<Integer>(list.size());
+    
+    int iterations = list.size();
+    for (int i = 0; i < iterations; i++) {
+      int randomIndex = (int) (Math.random() * list.size());
+      randomizedList.add(list.get(randomIndex));
+      list.remove(randomIndex);
+    }
+
+    return randomizedList;
+  }
+
+  // Finds the maximum value in a list
   public int findMaxValue(ArrayList<Integer> list) {
     int max = Integer.MIN_VALUE;
 
